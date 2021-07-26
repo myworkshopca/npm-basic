54a6f9f1cb2c651fb5be1293f018713d38d7a21a
Sean Chen <sean.chen@leocorn.com>
2021-07-26 04:31:00 +0000

    add comments and try multiple objects for a module.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-module.exports = require('./singleton.js');
+/**
+ * Check this post for module.exports and exports.
+ * - https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+ */
+//module.exports = require('./singleton.js');
+
+module.exports.basicSingleton = require('./singleton.js');
