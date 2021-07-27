7da23a36d1fd151994ba35d0f74a8db272d49fd6
Sean Chen <sean.chen@leocorn.com>
2021-07-27 18:43:26 +0000

    Get started the cases for Spread syntax

---
+++ src/spread.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Explore the spread syntax (...)
+ * 
+ * Spread syntax takes in an iterable (e.g an array)
+ * and expends it into individual elements.
+ * 
+ * Reference some post:
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ * - https://codeburst.io/what-are-three-dots-in-javascript-6f09476b03e1
+ */
+
+const spreadCases = {
+
+};
+
+module.exports = spreadCases;
